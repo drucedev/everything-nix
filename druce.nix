f07356c7ebef07da5ed8c9cb6d329e118b8d6e0a
Andrei Kukharau <contact@druce.dev>
2025-07-31 19:36:53 +0200

    Add wezterm config

--- druce.nix
+++ druce.nix
@@ -29,6 +29,21 @@
         wezterm = {
             enable = true;
             enableZshIntegration = true;
+            extraConfig = ''
+                local wezterm = require("wezterm")
+
+                local config = wezterm.config_builder()
+
+                config.font_size = 16
+
+                config.color_scheme = 'Catppuccin Mocha'
+
+                config.window_background_opacity = 0.85
+                config.macos_window_background_blur = 5
+                config.native_macos_fullscreen_mode = true
+
+                return config
+            '';
         };
 
         vscode = {
