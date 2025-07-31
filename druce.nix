adc2d8e79c7a96bbf81affe813986aada0054d64
Andrei Kukharau <contact@druce.dev>
2025-07-31 19:15:56 +0200

    Add zsh, starship, ghostty and nerd-font jetbrains-mono

--- druce.nix
+++ druce.nix
@@ -6,23 +6,47 @@
 
        username = "druce";
        homeDirectory = "/Users/druce";
+
+       shell.enableZshIntegration = true;
+
+       packages = with pkgs; [
+        nil
+        raycast
+       ];
     };
 
-    programs.vscode = {
-        enable = true;
-        profiles.default = {
-            extensions = with pkgs.vscode-extensions; [
-                jnoortheen.nix-ide
-            ];
-            userSettings = {
-                "nix.enableLanguageServer" = true;
-                "nix.serverPath" = "nil";
+    fonts.packages = with pkgs; [
+        nerd-fonts.jetbrains-mono
+    ];
+
+    programs = {
+        zsh = {
+            enable = true;
+        };
+
+        starship = {
+            enable = true;
+            enableZshIntegration = true;
+            settings = pkgs.lib.importTOML ./starship.toml;
+        };
+
+        ghostty = {
+            enable = true;
+            enableZshIntegration = true;
+        };
+
+        vscode = {
+            enable = true;
+            profiles.default = {
+                extensions = with pkgs.vscode-extensions; [
+                    jnoortheen.nix-ide
+                    tamasfe.even-better-toml
+                ];
+                userSettings = {
+                    "nix.enableLanguageServer" = true;
+                    "nix.serverPath" = "nil";
+                };
             };
         };
     };
-
-    home.packages = with pkgs; [
-        nil
-        raycast
-    ];
 }
