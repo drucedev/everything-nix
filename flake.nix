8f42dbe0c04957f328f454fd91a8be70ee2beba0
Andrei Kukharau <contact@druce.dev>
2025-09-19 21:11:47 +0200

    Make inputs shallow

--- flake.nix
+++ flake.nix
@@ -2,17 +2,17 @@
   description = "Durce's everything nix flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixos-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable?shallow=true";
+    nixos-stable.url = "github:NixOS/nixpkgs/nixos-25.05?shallow=true";
 
     # Nix darwin
-    darwin.url = "github:nix-darwin/nix-darwin/master";
+    darwin.url = "github:nix-darwin/nix-darwin/master?shallow=true";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
 
     # Home manager
-    hm-unstable.url = "github:nix-community/home-manager";
+    hm-unstable.url = "github:nix-community/home-manager?shallow=true";
     hm-unstable.inputs.nixpkgs.follows = "nixpkgs";
-    hm-stable.url = "github:nix-community/home-manager";
+    hm-stable.url = "github:nix-community/home-manager?shallow=true";
     hm-stable.inputs.nixpkgs.follows = "nixos-stable";
   };
 
