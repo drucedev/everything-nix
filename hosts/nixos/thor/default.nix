e718229d689586f3a2886ace9f5a4e9cfcdf0d1c
Andrei Kukharau <contact@druce.dev>
2025-09-28 17:49:49 +0200

    Enable ssh agent

--- hosts/nixos/thor/default.nix
+++ hosts/nixos/thor/default.nix
@@ -75,15 +75,16 @@
     #  wget
     fastfetch
     wezterm
-    kitty
     wofi
     git
+    brave
   ];
 
   fonts.enableDefaultPackages = true;
   fonts.packages = with pkgs; [ nerd-fonts.jetbrains-mono ];
 
   programs.hyprland.enable = true;
+  programs.ssh.startAgent = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
