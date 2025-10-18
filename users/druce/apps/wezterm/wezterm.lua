d69393f713dd883e77bd4a572132ee860e2f0e1d
Andrei Kukharau <contact@druce.dev>
2025-10-18 13:25:01 +0200

    Update wezterm config to match dotfiles

--- users/druce/apps/wezterm/wezterm.lua
+++ users/druce/apps/wezterm/wezterm.lua
@@ -1,13 +1,23 @@
-local wezterm = require("wezterm")
+local wezterm = require 'wezterm'
+local mux = wezterm.mux
+
+wezterm.on('gui-startup', function()
+    local window = mux.spawn_window({})
+    window:gui_window():maximize()
+end)
 
 local config = wezterm.config_builder()
 
-config.font_size = 16
-
+config.font_size = 18
 config.color_scheme = 'Catppuccin Mocha'
 
 config.window_background_opacity = 0.85
 config.macos_window_background_blur = 5
+
 config.native_macos_fullscreen_mode = true
+
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.tab_bar_at_bottom = true
 
 return config
