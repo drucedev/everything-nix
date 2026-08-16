6b95fecf2531633650e820ba931672aa8d09eb03
Andrei Kukharau <contact@druce.dev>
2026-08-16 18:48:58 +0200

    refactor: simplify nix settings

--- modules/nix.nix
+++ modules/nix.nix
@@ -6,15 +6,7 @@ let
   common =
     { ... }:
     {
-      nix.settings = {
-        substituters = [ "https://cache.nixos.org/" ];
-        trusted-public-keys = [
-          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        ];
-        experimental-features = "nix-command flakes";
-        fallback = true;
-        max-jobs = "auto";
-      };
+      nix.settings.experimental-features = "nix-command flakes";
 
       nix.optimise.automatic = true;
     };
